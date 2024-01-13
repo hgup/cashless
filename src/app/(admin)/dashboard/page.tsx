7d562b951f32d6cae7ac25f672b3ea4c75585a0d
Hursh Gupta <hurshgupta1@gmail.com>
2024-01-13 18:55:23 +0530

    initial setup

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Suspense } from "react"
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={` mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+    </main>
+  )
+}
---
+++ src/components/cashless-logo.tsx
@@ -0,0 +1,12 @@
+// import { lusitana } from '@/app/ui/fonts';
+
+import { IconJarLogoIcon } from "@radix-ui/react-icons"
+
+export default function CashlessLogo() {
+  return (
+    <div className={` flex flex-row items-center leading-none text-white`}>
+      <IconJarLogoIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Cashless</p>
+    </div>
+  )
+}
