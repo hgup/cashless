57fafbb400132b81e3dc70b1cdbd1b7864423367
Hursh Gupta <hurshgupta1@gmail.com>
2024-01-20 11:46:06 +0530

    pagination count

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,11 +1,14 @@
 "use server"
 
 import { z } from "zod"
-// import { sql } from "@vercel/postgres"
-import { revalidatePath } from "next/cache"
+import prisma from "./db"
+import { revalidatePath, unstable_noStore as noStore } from "next/cache"
 import { redirect } from "next/navigation"
 import { AuthError } from "next-auth"
 import { signIn } from "@/auth"
+import { $Enums, Room, UserRoles } from "@prisma/client"
+
+const ITEMS_PER_PAGE = 10
 
 export async function authenticate(
   prevState: string | undefined,
@@ -23,5 +26,117 @@ export async function authenticate(
       }
     }
     throw error
+  }
+}
+//  regd_no: string;
+//     roll_no: string;
+//     name: string;
+//     password: string;
+//     balance: number;
+//     room_no: $Enums.Room;
+//     class: string;
+//     status: $Enums.Status;
+//     photo: Buffer | null;
+//     role:
+
+const FormSchema = z.object({
+  regd_no: z.string(),
+  name: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  password: z.string({}),
+  balance: z.coerce
+    .number()
+    .gt(0, { message: "Please enter a number greater than $0 :/" }),
+  room_no: z.enum(
+    [
+      "A1",
+      "A2",
+      "A3",
+      "A4",
+      "A5",
+      "A6",
+      "A7",
+      "A8",
+      "B1",
+      "B2",
+      "B3",
+      "B4",
+      "B5",
+      "B6",
+      "B7",
+      "B8",
+      "C1",
+      "C2",
+      "C3",
+      "C4",
+      "C5",
+      "C6",
+      "C7",
+      "C8",
+      "S13",
+    ],
+    {
+      invalid_type_error: "Room doesn't exist",
+    }
+  ),
+  status: z.enum(["ACTIVE", "DISABLED"], {
+    invalid_type_error: "Please select an invoice status",
+  }),
+  role: z.enum([
+    "STUDENT",
+    "PHOTOCOPY",
+    "ACCOUNTANT",
+    "TEACHER_ADMIN",
+    "CENTRAL_ADMIN",
+  ]),
+})
+
+const UpdateStudent = FormSchema.omit({ regd_no: true })
+
+export async function updateStudent(regd_no: string, data: any) {
+  console.log(data)
+}
+
+export async function fetchInvoicesPages(query: string) {
+  noStore()
+  try {
+    const count = prisma.users.count({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: query,
+            },
+          },
+          {
+            roll_no: {
+              contains: query,
+            },
+          },
+          {
+            class: {
+              contains: query,
+            },
+          },
+          {
+            room_no: {
+              in: [query as Room],
+            },
+          },
+          {
+            role: {
+              in: [query as UserRoles],
+            },
+          },
+        ],
+      },
+    })
+
+    const totalPages = Math.ceil(Number(count) / ITEMS_PER_PAGE)
+    return totalPages
+  } catch (error) {
+    console.error("Database Error:", error)
+    throw new Error("Failed to fetch total number of invoices.")
   }
 }
