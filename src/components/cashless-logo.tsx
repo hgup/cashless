de484e9e281da4d759b001d8e8ebb468d6f100f8
Hursh Gupta <hurshgupta1@gmail.com>
2024-01-28 06:57:59 +0530

    logo lat

--- src/components/cashless-logo.tsx
+++ src/components/cashless-logo.tsx
@@ -2,16 +2,23 @@
 
 import { CurrencyRupeeIcon } from "@heroicons/react/24/outline"
 import { Card } from "./ui/card"
-import { CircleSlash2 } from "lucide-react"
-import { GitHubLogoIcon, NotionLogoIcon } from "@radix-ui/react-icons"
+import { CircleDotDashedIcon, CircleSlash2 } from "lucide-react"
+import {
+  CircleBackslashIcon,
+  GitHubLogoIcon,
+  NotionLogoIcon,
+} from "@radix-ui/react-icons"
 import React from "react"
 
-export default function CashlessLogo({ className }: React.PropsWithChildren) {
+export default function CashlessLogo({ className }: { className: string }) {
   return (
     <div
       className={`flex flex-row ${className} justify-center items-center aspect-square`}
     >
-      <CircleSlash2 fillRule="inherit" className="h-6 w-6 md:h-8 md:w-8  " />
+      <CircleDotDashedIcon
+        fillRule="inherit"
+        className="h-6 w-6 md:h-8 md:w-8  "
+      />
     </div>
   )
 }
